2fc95954b734d80a4b1cf9d4b15a6cf72a6915e0
jpalanca <jpalanca@dsic.upv.es>
2018-09-17 19:20:23 +0200

    added some tests.

--- tests/test_spade_bokeh.py
+++ tests/test_spade_bokeh.py
@@ -2,24 +2,41 @@
 # -*- coding: utf-8 -*-
 
 """Tests for `spade_bokeh` package."""
-
-import pytest
-
+from collections import namedtuple
 
 from spade_bokeh import spade_bokeh
 
-
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+agent = namedtuple("agent", "port")(port=1024)
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_server():
+    server = spade_bokeh.BokekServer(agent)
+
+    assert server.agent.port == 1024
+    assert not server.hostname
+    assert not server.port
+    assert not server.server
+    assert not server.is_running
+
+
+def test_add_plot():
+    server = spade_bokeh.BokekServer(agent)
+
+    server.add_plot("/my_plot", lambda x: x)
+
+    assert "/my_plot" in server.apps
+
+
+def test_get_plot_script():
+    server = spade_bokeh.BokekServer(agent)
+    server.hostname = "hostname"
+    server.port = 1024
+
+    server.add_plot("/my_plot", lambda x: x)
+
+    script = server.get_plot_script("/my_plot")
+
+    assert type(script) == str
+    assert script.startswith("\n<script ")
+    assert "bokeh-absolute-url=http://hostname:1024/my_plot" in script
+    assert script.endswith("</script>")
